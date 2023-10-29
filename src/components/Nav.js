bb280e193b0e91d4ac2fd6c909524e5f318af60e
Ard-it <ekoclickrepair@gmail.com>
2023-10-29 15:08:50 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,43 @@
 import React from 'react';
-
+import logo from '../images/Logo .svg';
 const Nav = () => {
     return (
         <nav>
-            Nav page
+            <a href='/'>
+                <img src={logo} alt='logo'/>
+            </a>
+
+            {/* Mobile Nav */}
+            <div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+
+            {/* Nav Items */}
+            <ul>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/'>About</a>
+                </li>
+                <li>
+                    <a href='/'>Services</a>
+                </li>
+                <li>
+                    <a href='/'>Menu</a>
+                </li>
+                <li>
+                    <a href='/'>Reservations</a>
+                </li>
+                <li>
+                    <a href='/'>Order Online</a>
+                </li>
+                <li>
+                    <a href='/'>Login</a>
+                </li>
+            </ul>
         </nav>
     )
 };
