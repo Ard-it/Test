4e84e6a63bbc85505e2514e2aa76074794320319
Ard-it <143628472+Ard-it@users.noreply.github.com>
2023-10-30 17:36:44 +0100

    HEADER

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <section>
+                {/* banner text */}
+            </section>
+
+        </header>
+
+    );
+};
+
+
+export default Header;  
